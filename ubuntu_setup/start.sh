ec51357aa23bf9eb2e2e23ac4874feaced0a893e
Robert Olechowski <robertolechowski@gmail.com>
2025-12-25 02:06:05 +0100

    notatki dodane

--- ubuntu_setup/start.sh
+++ ubuntu_setup/start.sh
@@ -1,31 +1,40 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo do-release-upgrade
 
-sudo apt install ntpdate ssh-import-id ca-certificates curl gnupg lsb-release tmux -y
+sudo apt install ntpdate gh ssh-import-id ca-certificates curl gnupg lsb-release tmux -y
 
 ssh-import-id-gh robertolechowski
 
 echo "alias cls="clear"" >> ~/.bashrc
+
+cat << 'EOF' >> ~/.bashrc
+alias ufw_status='sudo ufw status verbose'
+alias ps_docker='docker ps --format "table {{printf \"%.12s\" .ID}}\t{{printf \"%.25s\" .Names}}\t{{printf \"%.45s\" .Image}}\t{{.Status}}\t{{.Ports}}"'
+EOF
+
 source ~/.bashrc
 
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+sudo bash webmin-setup-repo.sh
+sudo apt-get install --install-recommends webmin usermin
+sudo systemctl status webmin
+sudo ufw allow 10000/tcp
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+sudo apt  install docker.io
+sudo apt install docker-compose-v2
 
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 
-sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
-docker plugin ls
+sudo ufw default deny incoming  
+sudo ufw default allow outgoing  
+sudo ufw allow OpenSSH  
+sudo ufw enable  
+sudo ufw status verbose
 
 sudo reboot
+
+docker login ghcr.io -u robertolechowski
+gh auth login
